687e0d26172552f54b408e55c3944abb378af78c
mataiodoxion <226399730+mataiodoxion@users.noreply.github.com>
2026-04-02 14:18:20 -0700

    Fix: login system; removed instance from git tracking

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -17,6 +17,18 @@ from flaskr.db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
+
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
@@ -69,3 +81,20 @@ def login():
         flash(error)
 
     return render_template("auth/login.html")
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("auth.login"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from flaskr.auth import login_required
+
 bp = Blueprint("dashboard", __name__)
 
 
 @bp.route("/")
+@login_required
 def index():
     return render_template("dashboard/index.html")
