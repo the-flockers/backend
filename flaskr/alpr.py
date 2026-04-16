0d562030e58b34f6c75399f2ef25687b3e85b5fc
mataiodoxion <226399730+mataiodoxion@users.noreply.github.com>
2026-04-16 13:27:13 -0700

    Fix CORS origin policy for alpr locations

--- flaskr/alpr.py
+++ flaskr/alpr.py
@@ -1,8 +1,10 @@
 # Interface for ALPR data
 
 from flask import Blueprint, send_file
+from flask_cors import CORS
 
 bp = Blueprint("alpr", __name__, url_prefix="/alpr")
+CORS(bp, origins=["https://deflock.opencodingsociety.com"])
 
 ALPR_LOCATIONS = "san_diego_alprs.geojson"
 
