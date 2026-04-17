10d29a9b74e8b87f18954620e8151a5d129555fc
mataiodoxion <226399730+mataiodoxion@users.noreply.github.com>
2026-04-17 13:37:57 -0700

    Allow local development mode

--- flaskr/alpr.py
+++ flaskr/alpr.py
@@ -1,10 +1,18 @@
 # Interface for ALPR data
 
+import os
+
 from flask import Blueprint, send_file
 from flask_cors import CORS
 
 bp = Blueprint("alpr", __name__, url_prefix="/alpr")
-CORS(bp, origins=["https://deflock.opencodingsociety.com"])
+
+# Allow localhost origins in development mode
+allowed_origins = ["https://deflock.opencodingsociety.com"]
+if os.getenv("DEV_MODE") == "true":
+    allowed_origins.extend(["http://127.0.0.1:4000", "http://localhost:4000"])
+
+CORS(bp, origins=allowed_origins)
 
 ALPR_LOCATIONS = "san_diego_alprs.geojson"
 
